38374d18e3ac5a354762b3b85676f4529a22b2ce
waqarakram93-tech <84813788+waqarakram93-tech@users.noreply.github.com>
2021-06-22 16:25:26 +0200

    added fucntion and local storage

--- app.js
+++ app.js
@@ -1,27 +1,69 @@
-function addTask() {
-    console.log("here");
-    const fs = require('fs');
-    const path = "record.json";
+// select Elements
 
-    if (fs.existsSync(path)) {
-        var inputItem = document.getElementById("itemInput").value;
-        const record = {
-            "id": 1,
-            "task": inputItem,
-            "status": "todo"
-        };
+const form = document.querySelector("#itemForm");
+const inputItem = document.querySelector("#itemInput");
+const itemsList = document.querySelector("#itemsList");
 
-        // convert JSON object to string
-        const data = JSON.stringify(record);
+//Items List
 
-        // write JSON string to a file
-        fs.writeFile('record.json', data, (err) => {
-            if (err) {
-                throw err;
-            }
-            console.log("JSON data is saved.");
-        });
-    } else {
-        console.log("DOES NOT exist:", path);
+let todoItems = [];
+
+const getList = function (todoItems) {
+    itemsList.innerHTML = "";
+    if (todoItems.length > 0) {
+        todoItems.forEach((item) => {
+            itemsList.insertAdjacentHTML("beforeend",
+                ` <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${item.name}</span>
+            <span>
+                <a><i class="bi bi-check-circle green"></i></a>
+                <a><i class="bi bi-pencil-square blue"></i></a>
+                <a><i class="bi bi-x-circle red"></i></a>
+            </span>
+        </li>`
+            )
+        })
     }
 }
+
+// get local storage from page
+const getLocalStorage = function () {
+    const todoStorage = localStorage.getItem("todoItems");
+    if (todoStorage === "undefined" || todoStorage === null) {
+        todoItems = [];
+    } else {
+        todoItems = JSON.parse(todoStorage);
+    }
+    console.log("items", todoItems);
+    getList(todoItems);
+}
+
+
+//set in Local storage
+const setLocalStorage = function (todoItems) {
+    localStorage.setItem("todoItems", JSON.stringify(todoItems));
+    location.reload();
+}
+
+
+// add item start
+document.addEventListener("DOMContentLoaded", () => {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const itemName = inputItem.value.trim();
+        if (itemName.length === 0) {
+            alert("Please enter book name");
+        } else {
+            const itemObj = {
+                name: itemName,
+                isDone: false,
+                addedAt: new Date().getTime(),
+            }
+            todoItems.push(itemObj);
+            setLocalStorage(todoItems);
+
+        }
+
+    })
+    getLocalStorage();
+})
